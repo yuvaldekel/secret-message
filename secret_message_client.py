a16e4e113a72646f6fb83481ab61df2e4f5d0fe3
Yuval Dekel <yuvddd05@gmail.com>
2024-04-25 18:49:34 +0300

    edit sr1

--- secret_message_client.py
+++ secret_message_client.py
@@ -1,16 +1,15 @@
 from scapy.all import IP, UDP, TCP, DNS, DNSQR, DNSRR
 from scapy.all import *
 
-CLIENT_IP = '172.26.25.218'
-SERVER_IP = '172.26.16.1'
+CLIENT_IP = '192.168.68.65'
+SERVER_IP = '192.168.68.62'
 
 def send_packet(packet):
     acknowledge = sr1(packet, timeout = 5)
-    try:
-        acknowledge = acknowledge[0]
+    if acknowledge != None:
         return False
-    except IndexError:
-        print('packet did not arrive trying again ')
+    else:
+        print('packet did not arrive')
         return True
 
 def main():
@@ -19,24 +18,26 @@ def main():
     for index, char in enumerate(message):
         ascii_presentation = ord(char)
 
-        continue_sent = True
-        message_packet = IP(dst = SERVER_IP, src = CLIENT_IP)/UDP(sport = 24601,dport = ascii_presentation, chksum = index - 1)
+        print(f"sending {char} to port {ascii_presentation}")
+
+        message_packet = IP(dst = SERVER_IP, src = CLIENT_IP)/UDP(sport = 24601,dport = ascii_presentation, chksum = index + 1)
         
-        continue_sent = send_packet(message_packet)
+        continue_sent = True
         i = 0
-        while continue_sent and i <= 10:
+        while continue_sent and i < 10:
             continue_sent = send_packet(message_packet)
             i = i + 1
 
-    continue_sent = True
-    end_packet = IP(dst = SERVER_IP, src = CLIENT_IP)/UDP(sport = 24601,dport = 4)
     
-    continue_sent = send_packet(end_packet)
-    while continue_sent and i <= 10:
-        i = 0
+    print("sending end message")
+
+    end_packet = IP(dst = SERVER_IP, src = CLIENT_IP)/UDP(sport = 24601,dport = 4)
+
+    continue_sent = True    
+    i = 0
+    while continue_sent and i < 10:    
         continue_sent = send_packet(end_packet)
         i = i + 1
         
-
 if __name__ == "__main__":
     main()
